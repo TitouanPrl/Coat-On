8dbef1b3751efb96a2fa7e7609fd5c6b490f9944
Titouan Pradal <88028169+TitouanPrl@users.noreply.github.com>
2024-06-21 15:59:16 +0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,5 @@
+# CoatOn
+## Setup
+1. Create an application.property file for your profile. <br>Example: `application-phil.properties`
+2. Set the environment variable `spring_profiles_active`. <br> Example: `export spring_profiles_active=phil`.
+3. Run application. `./mvnw spring-boot:run`
---
+++ webapi/src/main/java/com/esi/apigateway/util/JwtUtil.java
@@ -0,0 +1,30 @@
+package com.esi.apigateway.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    @Value("${secretKey}")
+    public static String SECRET;
+
+    /**
+     * Validate the authentication token
+     * 
+     * @param token the authentication token
+     */
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
